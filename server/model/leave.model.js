b18887315a7e479902b18f5efe506e263db92525
Ritik Raj <117502397+ritikraj07@users.noreply.github.com>
2025-09-24 11:26:51 +0530

    Update leave.model.js

--- server/model/leave.model.js
+++ server/model/leave.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const leaveSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    type: { type: String, enum: ["sick", "casual", "earned", "public"], required: true },
+    type: { type: String, enum: ["sick", "casual", "public"], required: true },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
