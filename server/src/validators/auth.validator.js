dfe6b571ea956d59b929a5671728909e32dd7e69
ritikraj07 <ritikra3rrr@gmail.com>
2026-02-28 17:34:07 +0530

    refactor: entire user data added in req.user

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -1,11 +1,23 @@
 const jwt = require('jsonwebtoken');
 const { JWT_Secret_Key } = require('../config');
+const Admin = require('../models/admin.model');
+const Employee = require('../models/employee.model');
 
 const createToken = (id) => {
     return jwt.sign({ id }, JWT_Secret_Key);
 }
 
-const verifyToken = (req, res, next) => {
+const getUser = async (id) => {
+  let user = await Admin.findById(id);
+  if (!user) {
+    user = await Employee.findById(id);
+    }
+    console.log("Log from auth validator \n",id,"\n" ,user)
+  return user;
+};
+
+
+const verifyToken = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(' ')[1];
         
@@ -18,6 +30,16 @@ const verifyToken = (req, res, next) => {
         
         const decoded = jwt.verify(token, JWT_Secret_Key);
         req.userId = decoded.id; // Set user ID for later use
+        
+        const user = await getUser(decoded.id);
+        if (!user) {
+            return res.status(401).send({ 
+                success: false,
+                message: "Invalid token" 
+            });
+        }
+        
+        req.user = user;
         next(); // Call next middleware
         
     } catch (error) {
